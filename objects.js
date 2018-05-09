c0b454511c122b2eb9c86feb947020b88b85317b
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-09 15:32:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,7 @@ let playlist = {
   'Lady Gaga' : 'Paparazzi'
 }
 
-
-
-
-var updatePlaylist = function(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, { [artistName]: songTitle })
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, { [artistName]: songTitle})
 }
+
