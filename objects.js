039b80c827c32fbf856b4eaab02bdbe894ce7d04
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-09 15:28:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ let playlist = {
 
 
 
-function updatePlaylist(object, key, value) {
-  return Object.assign({}, object, { [key]: value});
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, object, { [artistName]: songTitle});
 }
