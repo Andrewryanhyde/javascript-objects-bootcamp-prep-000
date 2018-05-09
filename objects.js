9fa7fb71243b4c946946bce3a77fe43cd0ce9b75
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-09 13:48:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,12 @@
 let playlist = {
-  LadyGaga: "Telephone"
-  Beyonce: "Halo"
+  LadyGaga: "Telephone",
+  Beyonce: "Halo",
   Muse: "Supermassive Black Hole"
 };
 
 function updatePlayList(playlist, artist, song) {
-  Object.assign({}, playlist {
+  Object.assign({}, playlist, {
     artist: song
-  }
+  })
   return playlist;
 }
